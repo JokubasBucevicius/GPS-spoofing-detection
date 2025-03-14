08d9c1bb041693f3f16799bdcd378fdba1a1f4dc
JokubasBucevicius <jokubz995@gmail.com>
2025-03-14 21:29:55 +0200

    Extended the workflow with new functions from Task A and B

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Main control file that joins the analysis together
 import timer_wraper as tw
 from data_loader import DataLoader
 from data_cleaner import DataCleaner
+from task_A import LocationAnomalyDetector
+from task_B import SpeedCourseAnomalyDetector
 
 def main():
     print("Loading data in parallel...")
@@ -15,6 +17,32 @@ def main():
     cleaner = DataCleaner()
     df_clean = cleaner.clean_data_parallel(df)
     print(f"Data Cleaned! Shape: {df_clean.shape}")
+
+    print("Running Task A: Location Anomalies Detection...")
+    anomaly_detector = LocationAnomalyDetector(df)
+    jump_anomalies, invalid_jumps = anomaly_detector.detect_location_anomalies_parallel()
+
+    print(f"\nLocation Anomalies Found: {len(jump_anomalies)}")
+    if not jump_anomalies.empty:
+        print(jump_anomalies.head())
+    
+    print(f"\nInvalid Coordinates Found: {len(invalid_jumps)}")
+    if not invalid_jumps.empty:
+        print(invalid_jumps.head())
+
+    print("Running Task B: Speed and Course Anomalies Detection...")
+    detector = SpeedCourseAnomalyDetector(df)
+    speed_anomalies, course_anomalies = detector.detect_anomalies_parallel()
+
+    print(f"\nSpeed Anomalies Found: {len(speed_anomalies)}")
+    if not speed_anomalies.empty:
+        print(speed_anomalies.head())
+
+    print(f"\nCourse Anomalies Found: {len(course_anomalies)}")
+    if not course_anomalies.empty:
+        print(course_anomalies.head())
+
+
 
     tw.print_execution_summary()
 
