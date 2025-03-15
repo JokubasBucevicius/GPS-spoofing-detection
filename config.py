0b7905e762555391ebe3d9c424d1750c1d97fdbe
JokubasBucevicius <jokubz995@gmail.com>
2025-03-15 12:42:53 +0200

    Added grid size variable for task C

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ CHUNK_SIZE = 10000  # Number of rows per chunk
 NUM_WORKERS = 8  # Adjust based on your machine
 DROP_COLUMNS = ["Type of mobile", "Callsign", "Ship type", "Draught", "Destination", "IMO", "Name", "Width", "Length", "Type of position fixing device", "Data source type", "A", "B", "C", "D", "ETA" ] # Column names that will be removed during data cleaning
 DATASET_SIZE = 100000 # Total size of the dataset (for small scale project only)
+GRID_SIZE = 0.4 #Grid cell size for TASK C
